534d977e87a6ecef48e80b463ca0d45cb7beca0c
Cadman <jcadiente505@gmail.com>
2018-06-03 22:28:40 -0400

    coded HTML elements and CSS styling

---
+++ assets/javascript/game.js
@@ -0,0 +1,24 @@
+
+// variables
+var randomNumber = [Math.floor(Math.random(100))];
+var healthPoints = 0;
+var attackPower = 0;
+var counterattackPower = 0;
+
+
+//functions
+function cloud() {
+    $("#body").css("background-image", "url('../assets/images/midgar.jpg.jpg')")
+}
+
+function squall() {
+
+}
+
+function tidus() {
+
+}
+
+function noctis() {
+
+}
