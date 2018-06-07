3affd40efb9a5e43741a552276659f3a5b87251d
Cadman <jcadiente505@gmail.com>
2018-06-06 21:49:16 -0400

    hidden all button elements, working on showing/hiding hero and enemy images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,17 @@
 
 // variables
-var heroChoice
-var enemyChoice
 var heroObj
 var enemyObj
+var heroChoice
+var enemyChoice
 var enemyCount
 var heroHp
 var enemyHp
 var heroAtk
 var enemyAtk
 var characterPortrait = $("<img>");
-    function startgame() {
+
+    $(document).ready(function() {
         var heroes;
         var enemies;
         enemyCount;
@@ -20,19 +21,19 @@ var characterPortrait = $("<img>");
             $(this).hide();
         });
 
-        enemies = $("#enemysection").find("button");
+        enemies = $("#enemyselector").find("button");
         enemies.each(function(index) {
             $(this).hide();
         });
 
-        enemyCount = $("#enemyselector").find("button");
+        enemyCount = $("#enemysection").find("button");
         enemyCount.each(function (index){
             $(this).hide();
         })
 
         $("#movessection").hide();
 
-    }
+    });
 
     startgame();
 
@@ -45,27 +46,58 @@ var characterPortrait = $("<img>");
         var enemiesLeft;
 
         pickedHero = $(this).attr("class");
-        $(this).addClass('selected');
+        $(this).addClass('hero');
 
         allHeroes = $("#heroselector").find("button");
         allHeroes.each(function (){
             $(this).hide();
         });
 
-        attacker = $("#herosection").find("button");
+        attacker = ($("#herosection")).find("button");
         attacker.each(function(){
-            if ($(this).hasClass('selected')){
+            if ($(this).hasClass('hero')){
                 $(this).show();
             }
         });
 
+        heroObj = herostats(pickedHero);
+            heroHp = heroObj.health;
+            heroAtk = heroObj.attacker;
+
+        $("#herosection").find("herostats").append("<li>" + "Health:" + heroHp);
+        $("#herosection").find("herostats").append("<li>" + "Attack:" + heroAtk);
+        
         enemiesLeft = $("#enemyselector").find("button");
 
             enemiesleft.each(function (){
-                if ($(this).hasClass(pickedHero)){
+                if (!$(this).hasClass(pickedHero)){
                     $(this).show();
-                }
-            })
+                };
+            });
     });
 
     // select enemy from #enemyselector, hides other images and reveals selected char in the #enemy section 
+
+    $("#enemyselector button").on("click", function(){
+        var enemyPicked;
+        var enemiesRemaining;
+
+        enemyPicked = $(this).attr("class");
+        $(this).addClass("enemy");
+
+        enemiesRemaining = $("#enemyselector").find("button");
+        enemiesRemaining.each(function (index){
+            if ($(this).hasClass("enemy")){
+                $(this).hide();
+            };
+        });
+
+        enemyCount = $("#enemyselector").find("button");
+        enemyCount.each(function (index){
+            if ($(this).hasClass("enemy")){
+                $(this).hide();
+            }
+        });
+
+        enemyObj = 
+    });
