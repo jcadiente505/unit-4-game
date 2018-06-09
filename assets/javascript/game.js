3743b8a9a4752b8bae07f814ae523b0bbcfdc83e
Cadman <jcadiente505@gmail.com>
2018-06-09 14:51:30 -0400

    finished game mechanics add audio files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,51 @@
 
-// variables
-var heroObj
-var enemyObj
-var heroChoice
-var enemyChoice
-var enemyCount
-var heroHp
-var enemyHp
-var heroAtk
-var enemyAtk
-var characterPortrait = $("<img>");
-
     $(document).ready(function() {
-        var heroes;
+        // variables
+        var enemy;
+        var hero;
+        var enemyCount;
+        var heroHp;
+        var enemyHp;
+        var heroAtk;
+        var enemyAtk;
+        var heroCounter;
+        var enemyCounter;
+        var pickedHero;
+        var enemyPicked;
+        var enemiesRemaining;
+        var allHeroes;
+        var enemiesLeft;
+        var heroeselector;
         var enemies;
-        enemyCount;
+        var squall = {
+            health: 140,
+            attack: 10,
+            counter: 8,
+            defeated: false
+        };
+        var cloud = {
+            health: 180,
+            attack: 8,
+            counter: 10,
+            defeated: false
+        };
+        var tidus = {
+            health: 130,
+            attack: 6,
+            counter: 15,
+            defeated: false
+        };
+        var noctis = {
+            health: 200,
+            attack: 5,
+            counter: 13,
+            defeated: false
+        };
+    
+        
         // Hides all images in each section until selected
-        heroes = $("#herosection").find("button");
-        heroes.each(function (index){
+        heroeselector = $("#herosection").find("button");
+        heroeselector.each(function (index){
             $(this).hide();
         });
 
@@ -29,75 +57,106 @@ var characterPortrait = $("<img>");
         enemyCount = $("#enemysection").find("button");
         enemyCount.each(function (index){
             $(this).hide();
-        })
+        });
 
         $("#movessection").hide();
 
-    });
-
-    startgame();
+    
 
 // select the character from #heroselector, hides other images, reveals selected char in #herosection
     $("#heroselector button").on("click", function(){
 
-        var pickedHero;
-        var allHeroes;
-        var attacker;
-        var enemiesLeft;
 
-        pickedHero = $(this).attr("class");
-        $(this).addClass('hero');
+        pickedHero = $(this).attr("id");
+        $("#hero-" + pickedHero).show();
+        console.log(pickedHero);
 
         allHeroes = $("#heroselector").find("button");
         allHeroes.each(function (){
             $(this).hide();
         });
 
-        attacker = ($("#herosection")).find("button");
-        attacker.each(function(){
-            if ($(this).hasClass('hero')){
-                $(this).show();
-            }
-        });
-
-        heroObj = herostats(pickedHero);
-            heroHp = heroObj.health;
-            heroAtk = heroObj.attacker;
-
-        $("#herosection").find("herostats").append("<li>" + "Health:" + heroHp);
-        $("#herosection").find("herostats").append("<li>" + "Attack:" + heroAtk);
-        
         enemiesLeft = $("#enemyselector").find("button");
-
-            enemiesleft.each(function (){
-                if (!$(this).hasClass(pickedHero)){
-                    $(this).show();
-                };
+        enemiesLeft.each(function (){
+            $(this).show();
             });
+
+        hero = charConstructor(pickedHero);
+		heroHp = hero.health;
+		heroAtk = hero.attack;
+        heroCounter = hero.counter;
+
+        $("#herosection").find("#herostats").append("<li>" + "Hero Health: " + "<span id = 'heroHealthStat'>" + heroHp + "</span>" + "</li>");
+        $("#herosection").find("#herostats").append("<li >" + "Hero Attack: " + "<span id = 'heroAttackStat'>" + heroAtk + "</span>" + "</li>");
+        $("#herosection").find("#herostats").append("<li>" + "Hero Counter Attack: " + "<span id = 'heroCounterStat'>" + heroCounter + "</span>" + "</li>");
+        
+        
     });
 
     // select enemy from #enemyselector, hides other images and reveals selected char in the #enemy section 
 
     $("#enemyselector button").on("click", function(){
-        var enemyPicked;
-        var enemiesRemaining;
+       
+        enemyPicked = $(this).attr("id");
+        $("#enemy-" + enemyPicked).show();
+        console.log("enemy picked", enemyPicked)
+    
+        enemiesRemaining = $("#enemyselector").find(enemyPicked)
+        $(this).hide();
 
-        enemyPicked = $(this).attr("class");
-        $(this).addClass("enemy");
+        enemy = charConstructor(enemyPicked);
+        console.log("second enemy picked", enemy);
+		enemyHp = enemy.health;
+		enemyAtk = enemy.attack;
+        enemyCounter = enemy.counter;
 
-        enemiesRemaining = $("#enemyselector").find("button");
-        enemiesRemaining.each(function (index){
-            if ($(this).hasClass("enemy")){
-                $(this).hide();
-            };
-        });
-
-        enemyCount = $("#enemyselector").find("button");
-        enemyCount.each(function (index){
-            if ($(this).hasClass("enemy")){
-                $(this).hide();
-            }
-        });
-
-        enemyObj = 
+        $("#enemysection").find("#enemystats").append("<li>" + "Enemy Health: " + "<span id = 'enemyHealthStat'>" + enemyHp + "</span>" + "</li>");
+        $("#enemysection").find("#enemystats").append("<li >" + "Enemy Attack: " + "<span id = 'enemyAttackStat'>" + enemyAtk + "</span>" + "</li>");
+        $("#enemysection").find("#enemystats").append("<li>" + "Enemy Counter Attack: " + "<span id = 'enemyCounterStat'>" + enemyCounter + "</span>" + "</li>");
     });
+
+    var charConstructor = function(character){
+
+        if(character === "cloud"){
+            return cloud;
+        }
+        else if(character === "squall"){
+            return squall;
+        }
+        else if(character === "tidus"){
+            return tidus;
+        }
+        else if(character === "noctis"){
+            return noctis; 
+        }   
+    }
+
+    $("#attack").on("click", function(){
+
+        heroHp = heroHp - enemyCounter;
+        console.log(enemyCounter)
+        console.log(heroHp)
+        enemyHp = enemyHp - heroAtk;
+    
+        heroAtk = heroAtk+6;
+    
+        $("#herosection").find("#herostats").html(`<ul><li> Hero Health: ${heroHp}</li> <li> Hero Attack: ${heroAtk}</li> <li> Hero Counter: ${heroCounter}</li></ul>`);
+        $("#enemysection").find("#enemystats").html(`<ul><li> Enemy Health: ${enemyHp}</li> <li> Enemy Attack: ${enemyAtk}</li> <li> Enemy Counter: ${enemyCounter}</li></ul>`);
+        
+        if(enemyHp <= 0){
+            enemy.defeated = true;
+            $("#enemysection").find(".character").hide();
+            alert("You Won!")
+        }
+    
+        else if(heroHp <= 0){
+            hero.defeated = true;
+            $("#herosection").find(".character").hide();
+            alert("You Lose")
+        }
+    });
+
+    $("#restart").on("click", function(){
+        location.reload()
+    })
+});
